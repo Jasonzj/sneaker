2358b9a3ab7a6ddbef5ff1106ace8be125b849c6
Jasonzj <925645402@qq.com>
2020-12-19 18:57:05 +0800

    test(button.test.tsx): add components/button test

---
+++ src/components/Button/Button.test.tsx
@@ -0,0 +1,37 @@
+import { render, screen } from '@testing-library/react'
+import Button from './Button'
+import { Props } from './type'
+
+const props: Props = {
+  text: 'Login In',
+  type: 'submit',
+  loading: false,
+}
+
+test('should take a snapshot', async () => {
+  const { asFragment } = render(<Button {...props} />)
+
+  expect(asFragment()).toMatchSnapshot()
+})
+
+test('should correct display text', () => {
+  render(<Button {...props} />)
+
+  expect(screen.getByText('Login In')).toBeInTheDocument()
+})
+
+test('should correct display type', () => {
+  render(<Button {...props} />)
+
+  expect(screen.getByRole('button')).toHaveProperty('type', 'submit')
+})
+
+test('should correct display loading status', () => {
+  const { rerender } = render(<Button {...props} />)
+
+  expect(screen.queryByText(/Loading/i)).not.toBeInTheDocument()
+
+  rerender(<Button {...props} loading={true} />)
+
+  expect(screen.getByText(/Loading/i)).toBeInTheDocument()
+})
