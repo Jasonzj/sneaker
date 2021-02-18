279dd580df3acfa3eef8398fc997fed381e58488
Jasonzj <925645402@qq.com>
2021-02-19 00:20:31 +0800

    fix(userequire.ts): fix race handling for manual requests

--- src/hooks/useLatestState.ts
+++ src/hooks/useLatestState.ts
@@ -1,9 +1,10 @@
 import { useRef, useEffect, MutableRefObject } from 'react'
 
-const useLatestState = <T>(value: T): MutableRefObject<T> => {
+const useLatestState = <T>(value: T, once = false): MutableRefObject<T> => {
   const ref = useRef<T>(value)
 
   useEffect(() => {
+    if (once) return
     ref.current = value
   })
 
